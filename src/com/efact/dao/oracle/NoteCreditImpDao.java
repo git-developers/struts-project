3dc05a78e1e40a3d1a64cdf0dd50d52919e7f239
Tianos <jafeth.bendezu.a@gmail.com>
2018-11-04 21:25:38 -0500

    Tianos said -> modifications made on: 2018-11-04 . 21:25:38

--- src/com/efact/dao/oracle/NoteCreditImpDao.java
+++ src/com/efact/dao/oracle/NoteCreditImpDao.java
@@ -199,7 +199,6 @@ public class NoteCreditImpDao extends OracleDaoFactory implements NoteCreditDao 
             st.setInt(5, object.getFechaVencimientoInt());
             st.setInt(6, object.getQueryTotal());
             st.setString(7, object.getQueryMoneyIntoWords());
-            //st.setString(7, object.getQueryNoteCreditType());
             
             st.setInt(8, object.getAfecto_1());
             st.setInt(9, object.getNoAfecto_1());
